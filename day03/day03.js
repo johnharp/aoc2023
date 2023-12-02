6b69c70869b256011334becf74174b6c043be5c8
John Harper <john@harper.link>
2023-12-02 00:30:08 -0600

    empty day03

---
+++ day03/day03.js
@@ -0,0 +1,9 @@
+import { readLines } from "../util/input.js";
+
+export function solve(filename) {
+    const lines = readLines(filename);
+
+    return [0, 0];
+}
+
+console.log(solve("day03/input-sample.txt"));
