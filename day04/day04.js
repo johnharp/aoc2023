237ba63e088b2fa1ff6dceb6541574fcc0b9d648
John Harper <john@harper.link>
2023-12-04 00:17:03 -0600

    day 04 complete

--- day04/day04.js
+++ day04/day04.js
@@ -3,12 +3,63 @@ import { readLines } from "../util/input.js";
 export function solve(filename) {
     const lines = readLines(filename);
 
-    const part1 = lines.length;
-    const part2 = 0;
+    const cards = [];
+    let part1 = 0;
+
+    for (const line of lines) {
+        const card = parseCard(line);
+        card.numMatches = countMatches(card);
+        cards.push(card);
+    }
+
+    for (const card of cards) {
+        if (card.numMatches > 0) {
+            part1 += 2 ** (card.numMatches-1);
+        }
+
+        for (let i = card.cardNum; (i < card.cardNum + card.numMatches) && (i < cards.length); i++) {
+            cards[i].copies += card.copies;
+        }
+    }
+
+    const part2 = cards.reduce((sum, card) => sum + card.copies, 0);
 
     return [part1, part2];
 }
 
+function parseCard(line) {
+    const [cardinfo, numberinfo] = line.split(":");
+    const [,cardnum] = cardinfo.split(/\s+/);
+    const [winningnums, mynums] = numberinfo.split("|");
 
+    return {
+        cardNum: Number(cardnum),
+        winningNumbers: parseNumbers(winningnums.trim()),
+        myNumbers: parseNumbers(mynums.trim()),
+        copies: 1,
+    }
+}
 
-console.log(solve("day04/input-sample.txt"));
+function parseNumbers(str) {
+    const nums = new Map();
+
+    for (const s of str.split(/\s+/)) {
+        const num = Number(s);
+        nums.set(num, true);
+    }
+
+    return nums;
+}
+
+function countMatches(card) {
+    let numMatches = 0;
+    for (const key of card.winningNumbers.keys()) {
+        if (card.myNumbers.has(key)) {
+            numMatches++;
+        }
+    }
+
+    return numMatches;
+}
+
+console.log(solve("day04/input.txt"));
