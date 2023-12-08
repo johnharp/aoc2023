4aa12bf3de216bf125a7968374bb2284f4a7148f
John Harper <john@harper.link>
2023-12-07 18:46:31 -0600

    empty day 08 template

---
+++ day08/day08.js
@@ -0,0 +1,12 @@
+import { readLines } from '../util/input.js';
+
+export function solve(filename) {
+    const lines = readLines(filename);
+
+    const part1 = lines.length;
+    const part2 = 0;
+
+    return [part1, part2];
+}
+
+console.log(solve('day08/input-sample.txt'));
