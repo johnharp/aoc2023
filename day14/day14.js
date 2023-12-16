9392a2ab0be9a39f55d73372f220c1a496857fee
John Harper <john@harper.link>
2023-12-15 23:28:15 -0600

    day 14 part 2 barely complete

--- day14/day14.js
+++ day14/day14.js
@@ -1,36 +1,145 @@
 import { readLines } from '../util/input.js';
 
 export function solve(filename) {
-
     const input = readLines(filename);
-    const grid = input.map(line => line.split(''));
+    const data = input.map((line) => line.split(''));
+    const grid = makeGrid(data);
 
-    tiltGridNorth(grid);
+    const loads = new Map();
+
+    grid.setOrientation('up');
+
+    const iter = 300;
+    for (let i = 0; i<iter; i++) {
+
+        tiltGrid(grid);
+        grid.setOrientation('left');
+        tiltGrid(grid);
+        grid.setOrientation('down');
+        tiltGrid(grid);
+        grid.setOrientation('right');
+        tiltGrid(grid);
+        grid.setOrientation('up');
+
+        const load = calcLoad(grid);
+        if (loads.has(load)) {
+            loads.set(load, loads.get(load).concat([i]));
+        } else {
+            loads.set(load, [i]);
+        }
+    }
+
+    // 95736
+
+
+    for (const [key, value] of loads.entries()) {
+        if (value.length > 1) {
+            const delta = value[value.length-1] - value[value.length-2];
+            const a = 1000000000 - iter;
+            const b = a % delta;
+
+            const last = value[value.length-1];
+
+            const num = (1000000000 - last - 1)/delta;
+
+            if (num % 1 === 0) {
+                console.log(`${key} -- Delta: ${delta},  ${last},  ${num}`);
+            }
+        }
+    }
 
 
     const part1 = calcLoad(grid);
-    const part2 = 0;
+    const part2 = calcLoad(grid);
 
     return [part1, part2];
 }
 
-function logGrid(grid) {
-    grid.forEach(row => console.log(row.join('')));
+function makeGrid(data) {
+    return {
+        data: data,
+        orientation: 'up',
+        setOrientation: function (orientation) {
+            this.orientation = orientation;
+        },
+        xp: function (x, y) {
+            const w = this.data[0].length;
+
+            if (this.orientation == 'up') {
+                return x;
+            } else if (this.orientation == 'down') {
+                return w-x-1;
+            } else if (this.orientation == 'left') {
+                return y;
+            } else if (this.orientation == 'right') {
+                return w-y-1;
+            }
+        },
+        yp: function (x, y) {
+            const h = this.data.length;
+
+            if (this.orientation == 'up') {
+                return y;
+            } else if (this.orientation == 'down') {
+                return h-y-1;
+            } else if (this.orientation == 'left') {
+                return h-x-1;
+            } else if (this.orientation == 'right') {
+                return x;
+            }
+        },
+        get: function(x, y) {
+            const xp = this.xp(x, y);
+            const yp = this.yp(x, y);
+            return data[yp][xp];
+        },
+        set: function (x, y, val) {
+            const xp = this.xp(x, y);
+            const yp = this.yp(x, y);
+            data[yp][xp] = val;
+        },
+        getWidth: function () {
+            return this.orientation == 'up' || this.orientation == 'down'
+                ? data[0].length
+                : data.length;
+        },
+        getHeight: function () {
+            return this.orientation == 'up' || this.orientation == 'down'
+                ? data.length
+                : data[0].length;
+        },
+        log: function() {
+            const w = this.getWidth();
+            const h = this.getHeight();
+            
+            for(let y = 0; y < h; y++) {
+                for(let x = 0; x < w; x++) {
+                    process.stdout.write(this.get(x, y));
+                }
+                process.stdout.write('\n');
+            }
+        },
+        toString: function() {
+            const str = data.map((line) => line.join('')).join('|');
+            return `${this.orientation}|${str}\n`;
+        }
+    };
 }
 
-function tiltGridNorth(grid) {
-    for (let col = 0; col < grid[0].length; col++) {
+function tiltGrid(grid) {
+
+    for (let x = 0; x < grid.getWidth(); x++) {
         let open = -1;
 
-        for (let row = 0; row < grid.length; row++) {
-            if (grid[row][col] === '.' && open === -1) {
-                open = row;
-            } else if (grid[row][col] === '#') {
+        for (let y = 0; y < grid.getHeight(); y++) {
+            if (grid.get(x, y) === '.' && open === -1) {
+                open = y;
+            } else if (grid.get(x, y) === '#') {
                 open = -1;
-            } else if (grid[row][col] === 'O') {
+            } else if (grid.get(x, y) === 'O') {
                 if (open > -1) {
-                    grid[open][col] = 'O';
-                    grid[row][col] = '.';
+                    grid.set(x, open, 'O');
+                    grid.set(x, y, '.');
                     open++;
                 }
             }
@@ -40,10 +149,10 @@ function tiltGridNorth(grid) {
 
 function calcLoad(grid) {
     let load = 0;
-    for (let row = 0; row < grid.length; row++) {
-        for (let col = 0; col < grid[0].length; col++) {
-            if (grid[row][col] === 'O') {
-                load += grid.length - row;
+    for (let y = 0; y < grid.getHeight(); y++) {
+        for (let x = 0; x < grid.getWidth(); x++) {
+            if (grid.get(x, y) === 'O') {
+                load += grid.getHeight() - y;
             }
         }
     }
