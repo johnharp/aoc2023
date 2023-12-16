cca4b294e996c8b4c5024c510249e015d3608687
John Harper <john@harper.link>
2023-12-16 01:10:27 -0600

    day 16 empty project

---
+++ day16/day16.js
@@ -0,0 +1,12 @@
+import { read } from '../util/input.js';
+
+export function solve(filename) {
+    const input = read(filename).split(",");
+
+    const part1 = 0;
+    const part2 = 0;
+
+    return [part1, part2];
+}
+
+console.log(solve('day16/input-sample.txt'));
